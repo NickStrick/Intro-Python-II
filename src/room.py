9cffdac697e5149b541e447361c2964370037849
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-04 18:41:24 -0500

    added functions for get and remove items for rooms

--- src/room.py
+++ src/room.py
@@ -3,6 +3,23 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
+
+    def setItem(self, item):
+        print('set down item')
+        if self.items == []:
+            self.items = [item]
+        else:
+            self.items = [*self.items, item]
+
+    def removeItem(self, item):
+        for i in self.items:
+            if item == i:
+                print('removed from room!')
+                self.items.remove(i)
+
+            else:
+                print('item isnt in list!!!')
