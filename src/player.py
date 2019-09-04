c8b1d5a9b181e698219c0cae4fff609cb6a2ef34
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-04 16:13:01 -0500

    created player constructor

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, currRoom):
+        self.room = currRoom
+        self.name = name
