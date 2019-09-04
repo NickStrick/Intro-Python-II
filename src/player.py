1c9ae9d78f5dd8f8727484adeb352d0237efefa5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-04 18:41:07 -0500

    added functions for get and remove items

--- src/player.py
+++ src/player.py
@@ -6,3 +6,21 @@ class Player:
     def __init__(self, name, currRoom):
         self.room = currRoom
         self.name = name
+        self.items = []
+
+    def grabItem(self, item):
+        print(self.items)
+        print(item)
+        if self.items == []:
+            self.items = [item]
+        else:
+            self.items = [*self.items, item]
+
+    def dropItem(self, item):
+        for i in self.items:
+            if item == i:
+                print('removed!')
+                self.items.remove(i)
+
+            else:
+                print('item isnt in list!!!')
