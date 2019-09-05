57115e09d23c9dfc2c4a4193a8330d25d56bcd77
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-05 14:12:00 -0500

    created item constructor, with functions for when its picked up or dropped

--- src/item.py
+++ src/item.py
@@ -1,5 +1,15 @@
 
 
 class Item:
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
+
+    def onTake(self):
+        print(f'you have picked up: {self.name}')
+
+    def onDrop(self):
+        print(f'you have dropped: {self.name}')
+
+    def look(self):
+        print(f'You looked at the {self.name}:\n    {self.description}')
