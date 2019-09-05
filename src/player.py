7ec9dbf5c89a91323b638e8f1d4107441ece1392
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-05 17:42:54 -0500

    added scoreing logic, and created changeRoom function

--- src/player.py
+++ src/player.py
@@ -10,6 +10,9 @@ class Player:
         self.items = []
 
     def grabItem(self, item):
+        if not item.found:
+            item.found == True
+            self.score += 15
         if self.items == []:
             self.items = [item]
         else:
@@ -20,5 +23,19 @@ class Player:
             if item == i:
                 self.items.remove(i)
 
-            else:
-                print('item isnt in list!!!')
+    def changeRoom(self, direction):
+
+        if hasattr(self.room, direction):
+            if direction == 'n_to':
+                self.room = self.room.n_to
+            elif direction == 's_to':
+                self.room = self.room.s_to
+            elif direction == 'e_to':
+                self.room = self.room.e_to
+            elif direction == 'w_to':
+                self.room = self.room.w_to
+            if not self.room.found:
+                self.score += 10
+                self.room.found = True
+        else:
+            print('you cant go that way!')
